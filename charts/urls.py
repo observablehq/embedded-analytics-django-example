2e5b8970d30561a7397fbe9a2d1539b564e0fcef
Michael Cooper <mythmon@gmail.com>
2024-11-05 14:33:45 -0800

    add continents

--- charts/urls.py
+++ charts/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("afr", views.continent("Africa", "AFR"), name="continent-afr"),
+    path("ame", views.continent("Americas", "AME"), name="continent-ame"),
+    path("asi", views.continent("Asia", "ASI"), name="continent-asi"),
+    path("eur", views.continent("Europe", "EUR"), name="continent-eur"),
+    path("oce", views.continent("Oceania", "OCE"), name="continent-oce"),
 ]
--- charts/views.py
+++ charts/views.py
@@ -12,14 +12,32 @@ def index(request):
     }
     return render(request, "charts/index.html", context)
 
+def continent(name, code):
+    def fn(request):
+        signed_url = sign_embedded_chart_url(f"https://observablehq.observablehq.cloud/olympian-embeds/continent/{code}/chart.js")
+        context = {
+            'medal_chart_url': signed_url,
+            'continent_name': name,
+            'continent_code': code,
+        }
+        return render(request, "charts/continent.html", context)
+    return fn
+
+SIGNATURE_ALIGN_MIN = 5
+SIGNATURE_VALIDITY_MIN = SIGNATURE_ALIGN_MIN * 2
+
 def sign_embedded_chart_url(url):
     parsed_url = urlparse(url)
+
+    nbf = datetime.datetime.now().timestamp()
+    nbf -= (nbf % (SIGNATURE_ALIGN_MIN * 60))
+    exp = nbf + (SIGNATURE_VALIDITY_MIN * 60)
+
     payload_data = {
         'sub': 'mythmon',
         'urn:observablehq:path': parsed_url.path,
-        'iat': int(datetime.datetime.now().timestamp()),
-        'nbf': int(datetime.datetime.now().timestamp()),
-        'exp': int((datetime.datetime.now() + datetime.timedelta(minutes=15)).timestamp()),
+        'nbf': int(nbf),
+        'exp': int(exp),
     }
     token = jwt.encode(payload_data, settings.EMBED_PRIVATE_KEY, algorithm='EdDSA')
     return f"{url}?token={token}"
